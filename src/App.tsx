68713640427d9e55c85eb9be7690ca84eeaa2cd5
ArkashJ <arkjain@bu.edu>
2023-03-13 10:14:24 -0400

    example shipper names

--- src/App.tsx
+++ src/App.tsx
@@ -110,7 +110,7 @@ const App = () => {
         </Grid>
         <Grid xs={12} md={3}>
           <Typography variant="h5" gutterBottom>
-            Select a Shipper Name (Eg - Etsy, Amazon, KashMoney)
+            Select a Shipper Name (Eg - Etsy, Shopify, KashMoney)
           </Typography>
           <div style={{ width: "100%" }}>
             <form onSubmit={(e) => handleSubmit(e)}>
