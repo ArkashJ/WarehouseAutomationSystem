8ea550ee9f77984104cb14d6793954afb87b312f
ArkashJ <arkjain@bu.edu>
2023-03-14 19:17:51 -0400

    adding swagger

--- shell.sh
+++ shell.sh
@@ -1,4 +1,6 @@
 brew update
 az login
 az account set --subscription cff83f95-63c7-44d3-80f8-26251ff8f8ec
-az functionapp keys set -g Warehouse -n functionInventory --key-type functionKeys --key-name customerLogin
+az functionapp keys set -g Warehouse -n functionInventory --key-type functionKeys --key-name apim-functionInventory-apim
+
+az apim key-rotate --name <apim-instance-name> --resource-group <resource-group-name> --key-type primary
